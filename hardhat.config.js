fed6df227afc87f6512ba0da3b48d6ba25dc1fa5
Madmin27 <admin@nizip.com>
2024-04-09 01:20:19 +0300

    Update hardhat.config.js
    mantle sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,7 +11,7 @@ require("hardhat-deploy");
  */
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "";
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+// const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
@@ -19,16 +19,16 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
-    hardhat: {
-      chainId: 31337,
+//    hardhat: {
+ //     chainId: 31337,
       // gasPrice: 130000000000,
-    },
-    sepolia: {
-      url: SEPOLIA_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 11155111,
+ //   },
+      "mantlesepolia": {
+      url: "https://rpc.sepolia.mantle.xyz/",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 5003,
       blockConfirmations: 6,
-    },
+    }
   },
   solidity: {
     compilers: [
